c925504652023032d51b6c3ee0ec98078f2dca00
paulobr4z <paulobrazaraujo@gmail.com>
2024-10-01 17:02:08 -0300

    Setup do Projeto - Instalando shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
-    <div>
-      <h1>Hello World!!</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
     </div>
   )
 }
